14f96492f44af152dfdd48051f151af74da748b7
Labibi11 <riskyhabibie65@gmail.com>
2025-07-10 20:20:00 +0800

    tambahan pada home

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -4,6 +4,7 @@ import {View, Text, Image, TouchableOpacity, ScrollView} from 'react-native';
 import ImageHeader from '../components/ImageHeader';
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import Geolocation from '@react-native-community/geolocation';
 const kamar = require('./../assets/kamar.jpg');
 
 function Home() {
@@ -31,6 +32,43 @@ function Home() {
 
     fetchPenginapan();
   }, []);
+  
+  const [userLat, setUserLat] = useState(0);
+  const [userLong, setUserLong] = useState(0);
+
+  const getCurrentLocation = () => {
+    Geolocation.getCurrentPosition(info => {
+      setUserLat(info.coords.latitude);
+      setUserLong(info.coords.longitude);
+    });
+  };
+
+  useEffect(() => {
+    getCurrentLocation();
+  }, []);
+
+  const getDistance = (lat1Deg, lon1Deg, lat2Deg, lon2Deg) => {
+    const toRad = degree => {
+      return (degree * Math.PI) / 180;
+    };
+
+    const lat1 = toRad(lat1Deg);
+    const lon1 = toRad(lon1Deg);
+    const lat2 = toRad(lat2Deg);
+    const lon2 = toRad(lon2Deg);
+
+    const {sin, cos, sqrt, atan2} = Math;
+
+    const R = 6371;
+    const dLat = lat2 - lat1;
+    const dLon = lon2 - lon1;
+    const a =
+      sin(dLat / 2) * sin(dLat / 2) +
+      cos(lat1) * cos(lat2) * sin(dLon / 2) * sin(dLon / 2);
+    const c = 2 * atan2(sqrt(a), sqrt(1 - a));
+    const d = R * c;
+    return d;
+  };
 
   return (
     <ScrollView style={{flex: 1, backgroundColor: '#efefef'}}>
@@ -77,7 +115,9 @@ function Home() {
             }}>
             <View>
               <Image
-                source={{uri: 'http://192.168.43.6/api-test/admin/'+item.foto}}
+                source={{
+                  uri: 'http://192.168.43.6/api-test/admin/' + item.foto,
+                }}
                 style={{width: 100, height: 100, borderRadius: 20}}
               />
             </View>
@@ -87,6 +127,10 @@ function Home() {
               </Text>
               <Text numberOfLines={1} style={{fontSize: 17}}>
                 {item.alamat}
+              </Text>
+              <Text numberOfLines={1} style={{fontSize: 17}}>
+                {getDistance(userLat, userLong, item.lat, item.lng).toFixed(1)}{' '}
+                km
               </Text>
             </View>
           </TouchableOpacity>
